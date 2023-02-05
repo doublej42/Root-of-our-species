b6329116d2a2d026aa4b1962f6c527aee5e52af2
Jeff Jacob <phansoft@gmail.com>
2023-02-04 16:26:37 -0800

    Asteroids explode and head for the player

--- Assets/Scripts/AsteroidScript.cs
+++ Assets/Scripts/AsteroidScript.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.Serialization;
 using UnityEngine;
 
 public class AsteroidScript : MonoBehaviour
@@ -7,27 +9,70 @@ public class AsteroidScript : MonoBehaviour
     [SerializeField]
     private GameObject SmallerGameObject = null;
 
+    [SerializeField]
+    public float angularVelocity = 50f;
+
+    [SerializeField]
+    public int size;
+ 
+
+
+    //[SerializeField]
+    //public float speed = 1.0f;
+
+    //public Vector2 velocity;
+
+    private float invulnerableTime = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        var rigid = GetComponent<Rigidbody2D>();
+        //rotate it to a random spot and start it spinning
+        transform.Rotate(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+        rigid.angularVelocity = angularVelocity;
+        //Debug.Log($"Asteroid Up {transform.up.x}, {transform.up.y}, {transform.up.z}");
+        //rigid.velocity = velocity;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        invulnerableTime -= Time.deltaTime;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (invulnerableTime > 0)
+        {
+            return;
+        }
         Debug.Log($"Astroid Hit {collision.gameObject.name}");
+        if (collision.gameObject.layer == gameObject.layer)
+        {
+
+            if(collision.gameObject.TryGetComponent<AsteroidScript>(out var targetScript) && targetScript.size < size)
+            {
+                return; //ignore hitting smaller items;
+            }
+        }
         if(SmallerGameObject != null)
         {
-            var numberOfChilderen = Random.Range(1, 4);
-            for(var i = 0; i < numberOfChilderen;i++) {
-                Instantiate(SmallerGameObject, transform.position, transform.rotation, transform.parent);
+            var numberOfChilderen = Random.Range(2, 6);
+            var ship = GameObject.FindGameObjectWithTag("Player");
+            float radius = 1f;
+    
+            for (var i = 0; i < numberOfChilderen;i++) {
+                float angle = i * Mathf.PI * 2f / numberOfChilderen;
+                var newPos = transform.position + new Vector3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius,0);
+                var newItem = Instantiate(SmallerGameObject, newPos, transform.rotation, transform.parent);
+     
+                var vectorToShip3D = (ship.transform.position - newPos).normalized;
+                var vectorToShip2D = new Vector2(vectorToShip3D.x, vectorToShip3D.y);
+                // newItem.GetComponent<Rigidbody2D>().velocity = (collision.relativeVelocity + vectorToShip2D);
+                //newItem.GetComponent<Rigidbody2D>().velocity = (collision.relativeVelocity );
+                newItem.GetComponent<Rigidbody2D>().velocity = vectorToShip2D;
             }
         }
         Destroy(gameObject);
--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class AsteroidSpawner : MonoBehaviour
@@ -9,16 +10,36 @@ public class AsteroidSpawner : MonoBehaviour
     [SerializeField]
     private GameObject Ship;
 
+    [SerializeField]
+    private float frequency = 1.0f;
+    private float spawnTimeLeft = 0;
+
+    [SerializeField]
+    public float speed = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(ToSpawn, new Vector3(Ship.transform.position.x+3, Ship.transform.position.y+3, 0), Ship.transform.rotation, transform);
+        //spawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        spawnTimeLeft -= Time.deltaTime;
+        if (spawnTimeLeft <= 0)
+        {
+            spawnTimeLeft = frequency;
+            spawn();
+        }
+    }
+
+    private void spawn()
+    {
+        float radius = 20f;
+        float angle = Random.Range(0, Mathf.PI * 2);
+        var newPos = transform.position + new Vector3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius, 0);
+        var newItem = Instantiate(ToSpawn, newPos, Ship.transform.rotation);
+        newItem.GetComponent<Rigidbody2D>().velocity = (Ship.transform.position - newPos).normalized * speed;
     }
 }
